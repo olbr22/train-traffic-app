7bfa8d08d3a804d82e23b05778dae7126e7630e3
Olha Bruce <olbr22@student.bth.se>
2024-04-21 08:43:46 +0200

    Added a new paragraph to render()

--- components/error-handler.js
+++ components/error-handler.js
@@ -8,6 +8,7 @@ export default class ErrorHandler extends HTMLElement {
             <div class="error-message">
                 <h1>Oops! Something went wrong</h1>
                 <p>Sorry, we are unable to load the data at the moment. Please try again later.</p>
+                <p>Sorry, we are unable to load the data at the moment. Please try again later.</p>
             </div>
         `;
     }
